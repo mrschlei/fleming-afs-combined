b603668c25a1d8a5d9d4eb8918958b1ab7751a17
Jeff Cousineau <cousinea@umich.edu>
2018-08-17 10:56:56 -0400

    initial commit - combo branch

--- start.sh
+++ start.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
 
 # Redirect logs to stdout and stderr for docker reasons.
-ln -sf /dev/stdout /var/log/httpd/access_log
-ln -sf /dev/stderr /var/log/httpd/error_log
+ln -sf /dev/stdout /var/log/apache2/access_log
+ln -sf /dev/stderr /var/log/apache2/error_log
 
 # apache and virtual host secrets
-ln -sf /secrets/httpd/httpd.conf /etc/httpd/conf/httpd.conf
-ln -sf /secrets/httpd/cosign.conf /etc/httpd/conf.d/cosign.conf
+ln -sf /secrets/apache2/apache2.conf /etc/apache2/apache2.conf
+ln -sf /secrets/apache2/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
 
-# SSL secrets
-ln -sf /secrets/ssl/USERTrustRSACertificationAuthority.pem /etc/pki/tls/certs/USERTrustRSACertificationAuthority.pem
-ln -sf /secrets/ssl/AddTrustExternalCARoot.pem /etc/pki/tls/certs/AddTrustExternalCARoot.pem
-ln -sf /secrets/ssl/sha384-Intermediate-cert.pem /etc/pki/tls/certs/sha384-Intermediate-cert.pem
-
-if [ -f /secrets/app/local.start.sh ]
+# If it exists, include local.start.sh
+if [ -f /secrets/start/local.start.sh ]
 then
-  /bin/sh /secrets/app/local.start.sh
+  /bin/sh /secrets/start/local.start.sh
 fi
 
-## Rehash command needs to be run before starting apache.
-c_rehash /etc/pki/tls/certs >/dev/null
+# If it default folder doesn't exist, copy template
+# modules and themes to the persistent volume.
+if [! -f /var/www/html/sites/default/settings.php ]
+then
+  mv -R /tmp/sites/ /var/www/html/
+fi
 
-#drush @sites cc all --yes
-#drush up --no-backup --yes
+a2enmod ssl
+a2enmod include
+a2ensite default-ssl 
 
-#while true; do sleep 2; done
+/usr/local/bin/apache2-foreground
 
-/usr/sbin/httpd -DFOREGROUND -f "/etc/httpd/conf/httpd.conf"
